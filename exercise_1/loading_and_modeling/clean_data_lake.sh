1b1604c33f135d81664f395ad7253b530920539d
w205 <w205@ip-172-31-12-164.ec2.internal>
2017-03-10 18:42:45 +0000

    load_data_lake.sh and clean_data_lake.sh

---
+++ exercise_1/loading_and_modeling/clean_data_lake.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# remove staging directory and files
+rm ~/staging/exercise_1/*
+rmdir ~/staging/exercise_1
+
+# remove hdfs files and directory
+
+
+hdfs dfs -rm /user/w205/hospital_compare/hospitals/hospitals.csv
+hdfs dfs -rm /user/w205/hospital_compare/effective_care/effective_care.csv
+hdfs dfs -rm /user/w205/hospital_compare/readmission/readmission.csv
+hdfs dfs -rm /user/w205/hospital_compare/measures/measures.csv
+hdfs dfs -rm /user/w205/hospital_compare/surveys_responses/surveys_responses.csv
+
+hdfs dfs -rmdir /user/w205/hospital_compare/hospitals
+hdfs dfs -rmdir /user/w205/hospital_compare/effective_care
+hdfs dfs -rmdir /user/w205/hospital_compare/readmission
+hdfs dfs -rmdir /user/w205/hospital_compare/measures
+hdfs dfs -rmdir /user/w205/hospital_compare/surveys_responses
+
+hdfs dfs -rmdir /user/w205/hospital_compare
+# clean exit
+exit
+
